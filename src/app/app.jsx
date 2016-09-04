3453d4bb2fcbd40a890a4c30756ff919048eb701
Flora <xyan19>
2016-09-04 02:54:57 -0500

    Add new question form

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -6,8 +6,22 @@ import AppBar from 'material-ui/AppBar';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
 import $ from 'jquery';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 
 injectTapEventPlugin();
+
+var styles = {
+    addButton: {
+	marginRight: 20,
+	marginBottom: 20,
+	bottom: 0,
+	right: 0,
+	position: 'absolute'
+    }
+}
 
 var Kids = React.createClass({
     getInitialState: function() {
@@ -26,6 +40,20 @@ var Kids = React.createClass({
 	    }.bind(this)
 	});
     },
+    handleKidSubmit: function(kid) {
+	$.ajax({
+	    url: this.props.url,
+	    dataType: 'json',
+	    type: 'POST',
+	    data: kid,
+	    success: function(data) {
+		this.setState({data: data});
+	    }.bind(this),
+	    error: function(xhr, status, err) {
+		console.error(this.props.url, status, err.toString());
+	    }.bind(this)
+	});
+    },
     componentDidMount: function() {
 	this.loadKidsFromServer();
 	setInterval(this.loadKidsFromServer, 2000);
@@ -33,7 +61,7 @@ var Kids = React.createClass({
     render: function() {
 	return (
 	    <div className="Kids">
-		<KidsList data={this.state.data} />
+		<KidsList data={this.state.data} onKidSubmit={this.handleKidSubmit}/>
 	    </div>
 	);
     }
@@ -48,8 +76,91 @@ var KidsList = React.createClass({
 	});
 	return (
 	    <div className="kidsList">
-	    {kidsNodes}
+		{kidsNodes}
+		<AddKid onKidSubmit={this.props.onKidSubmit}/>
 	    </div>
+	);
+    }
+});
+
+var AddKid = React.createClass({
+    getInitialState: function() {
+	return {name: '', room: '', question:'', id:'', expanded:false};
+    },
+    handleNameChange: function(e) {
+	this.setState({name: e.target.value});
+    },
+    handleRoomChange: function(e) {
+	this.setState({room: e.target.value});
+    },
+    handleQuestionChange: function(e) {
+	this.setState({question: e.target.value});
+    },
+    handleIdChange: function(e) {
+	this.setState({id: e.target.value});
+    },
+    handleExpandChange: function(expanded) {
+	this.setState({expanded: expanded})
+    },
+    reduce: function() {
+	this.setState({expanded: false})
+    },
+    submitKid: function(e) {
+	this.props.onKidSubmit({
+	    name: this.state.name.trim(),
+	    room: this.state.room.trim(),
+	    question: this.state.question.trim(),
+	    id: this.state.id.trim()
+	});
+	this.setState({name: '', room: '', question:'', id:''});
+	this.reduce();
+    },
+    render: function() {
+	return (
+	    <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+		<CardHeader
+		    title="New Question"
+		    subtitle="Expand to add!"
+		    actAsExpander={true}
+		    showExpandableButton={true}
+		/>
+		<CardText expandable={true}>
+		    <TextField
+			hintText="Name"
+			errorText={this.state.name ? "" : "This field is required"}
+			value={this.state.name}
+			onChange={this.handleNameChange}
+		    />
+		    <br/>
+		    <TextField
+			hintText="Room"
+			errorText={this.state.room ? "" : "This field is required"}
+			value={this.state.room}
+			onChange={this.handleRoomChange}
+		    />
+		    <br/>
+		    <TextField
+			hintText="Question"
+			errorText={this.state.question ? "" : "This field is required"}
+			value={this.state.question}
+			onChange={this.handleQuestionChange}
+		    />
+		    <br/>
+		    <TextField
+			hintText="NetID"
+			errorText={this.state.id ? "" : "This field is required"}
+			value={this.state.id}
+			onChange={this.handleIdChange}
+		    />
+		    <br/>
+		    <CardActions>
+			<FlatButton
+			    label="Submit" disabled={!(this.state.name && this.state.room && this.state.question && this.state.id)}
+			    onTouchTap={this.submitKid}
+			/>
+		    </CardActions>
+		</CardText>
+	    </Card>
 	);
     }
 });
@@ -72,6 +183,12 @@ const QAppBar = () => (
     <AppBar
 	title="q.cs"
     />
+);
+
+const AddButton = () => (
+    <FloatingActionButton secondary={true} style={styles.addButton}>
+	<ContentAdd />
+    </FloatingActionButton>
 );
 
 var App = React.createClass({
