be260d22afc219c65c01159943b9c74e09d8f821
Thomas Liu <thomasliu02@gmail.com>
2016-10-29 18:06:28 -0500

    es6ify announcement

--- frontend/src/app/Kids/Announcement.jsx
+++ frontend/src/app/Kids/Announcement.jsx
@@ -9,20 +9,32 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 
 
-var Announcement = React.createClass({
-    render: function() {
+class Announcement extends React.Component {
+    render() {
+	var announcementAvatar = (
+	    <Avatar icon={<ActionAnnouncement />}
+		    backgroundColor={this.props.muiTheme.palette.accent1Color}
+	    />
+	);
+	
 	return (
 	    <ListItem primaryText={<strong>Announcement</strong>}
-		      secondaryText={<p><span style={{color: darkBlack}}>{this.props.message}</span></p>}
-		      leftAvatar={<Avatar icon={<ActionAnnouncement />}
-					  backgroundColor={this.props.muiTheme.palette.accent1Color} />}
+		      secondaryText={
+			  <p>
+			      <span style={{color: darkBlack}}>
+					  {this.props.message}
+			      </span>
+			  </p>}
+		      leftAvatar={announcementAvatar}
 		      disabled={true}
 		      rightIconButton={this.props.instructor ?
-				       <IconButton onTouchTap={this.props.onRemove}><ActionDelete /> </IconButton> :
+				       <IconButton onTouchTap={this.props.onRemove}>
+					   <ActionDelete />
+				       </IconButton> :
 				       null}
 	    />
 	);
     }
-});
+}
 
 export default muiThemeable()(Announcement);
