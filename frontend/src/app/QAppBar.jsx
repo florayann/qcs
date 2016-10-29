de4422c4604242bec419995754fc88c0b2db3afc
Thomas Liu <thomasliu02@gmail.com>
2016-10-29 15:40:05 -0500

    es6ify QAppBar

--- frontend/src/app/QAppBar.jsx
+++ frontend/src/app/QAppBar.jsx
@@ -6,12 +6,16 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 
-var QAppBarMenu = React.createClass({
-    render: function () {
+
+class QAppBarMenu extends React.Component {
+    render() {
 	return (
-	    <IconMenu iconButtonElement={<IconButton><MoreVertIcon color={white}/></IconButton>}
-		      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+	    <IconMenu anchorOrigin={{horizontal: 'right', vertical: 'top'}}
 		      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+		      iconButtonElement={
+			  <IconButton>
+			      <MoreVertIcon color={white}/>
+			  </IconButton>}
 	    >
 		<MenuItem primaryText="Refresh" onTouchTap={this.props.onRefresh}/>
 		<MenuItem primaryText="Log out"
@@ -20,18 +24,21 @@ var QAppBarMenu = React.createClass({
 	    </IconMenu>
 	);
     }
-});
+}
 
-var QAppBar = React.createClass({
-    render: function () {
+
+class QAppBar extends React.Component {
+    render() {
 	return (
-	    <AppBar
-		title={this.props.queueName}
-		onLeftIconButtonTouchTap={this.props.onLeftIconButtonTouchTap}
-		iconElementRight={<QAppBarMenu onLogout={this.props.onLogout} onRefresh={this.props.onRefresh}/>}
+	    <AppBar title={this.props.queueName}
+		    iconElementRight={<QAppBarMenu onRefresh={this.props.onRefresh}
+						   onLogout={this.props.onLogout}
+				      />}
+		    onLeftIconButtonTouchTap={this.props.onLeftIconButtonTouchTap}
 	    />
 	);
     }
-});
+}
+
 
 export default QAppBar;
