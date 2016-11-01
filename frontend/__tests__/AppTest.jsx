a1dc3ecacd55fa9926ab93c2f9cd33af2d9cf633
Thomas Liu <thomasliu02@gmail.com>
2016-10-31 23:05:47 -0500

    Add request handling tests

--- frontend/__tests__/AppTest.jsx
+++ frontend/__tests__/AppTest.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import App from '../src/app/app';
 import $ from 'jquery';
+import sinon from 'sinon';
 
 jest.unmock('../src/app/app');
 
@@ -43,18 +44,54 @@ describe('select queue', () => {
     });
 });
 
-describe('request failure handling', () => {
-    const app = shallow(<App class_url="/classes"
-			     queue_url="/queue/"
-			     queues_url="/class/"
-			     login_url="/auth"
-			/>);
+describe('request handling', () => {
+    afterEach(() => {
+	jest.clearAllMocks();
+    });
 
     it('marks as not instructor on 403', () => {
+	const app = shallow(<App class_url="/classes"
+				 queue_url="/queue/"
+				 queues_url="/class/"
+				 login_url="/auth"
+			    />);
 	app.setState({queueInstructor: true});
 	app.instance().handleSelectQueue('1', 'CS 233');
 	$.ajax.mock.calls[0][0].error({status: 403});
 	expect(app.state().queueInstructor).toEqual(false);
+    });
+
+    it('retries loading classes if disconnected', () => {
+	sinon.spy(App.prototype, 'componentDidMount');
+	mount(<App class_url="/classes"
+		   queue_url="/queue/"
+		   queues_url="/class/"
+		   login_url="/auth"
+	      />);
+	jest.useFakeTimers();
+	expect(App.prototype.componentDidMount.calledOnce).toEqual(true);
+	$.ajax.mock.calls[0][0].success('user');
+	$.ajax.mock.calls[1][0].error({status: 404}, 'error', 'Not Found');
+	expect(setTimeout.mock.calls.length).toBe(1);
+	jest.runOnlyPendingTimers();
+	$.ajax.mock.calls[2][0].error({status: 404}, 'error', 'Not Found');
+	expect(setTimeout.mock.calls.length).toBe(2);
+    });
+
+    it('logs in and logs out', () => {
+	const app = shallow(<App class_url="/classes"
+				 queue_url="/queue/"
+				 queues_url="/class/"
+				 login_url="/auth"
+			    />);
+	app.instance().handleLogin('user');
+	$.ajax.mock.calls[0][0].success('user');
+	expect(app.find('QAppBar').length).toBe(1);
+	app.instance().handleLogout();
+	$.ajax.mock.calls[1][0].success();
+	expect(app.state().username).toBe(null);
+	app.update();
+	expect(app.find('QAppBar').length).toBe(0);
     });
 });
 
