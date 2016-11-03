786dbfd34c2e5c8d21130674acd80bcf8b1fc1ff
Thomas Liu <thomasliu02@gmail.com>
2016-11-03 04:16:53 -0500

    Cover other case in AnnouncementTest

--- frontend/__tests__/Kids/AnnouncementTest.jsx
+++ frontend/__tests__/Kids/AnnouncementTest.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import IconButton from 'material-ui/IconButton';
 import Announcement from '../../src/app/Kids/Announcement';
 
 jest.unmock('../../src/app/Kids/Announcement');
@@ -14,5 +15,15 @@ describe('appearance', () => {
 						   muiTheme={muiTheme}
 				     />);
 	expect(announcement.find('ListItem').props().rightIconButton).toEqual(null);
+    });
+
+    it('does shows delete button for instructors', () => {
+	const announcement = shallow(<Announcement instructor={true}
+						   message="hi"
+						   onRemove={() => {}}
+						   muiTheme={muiTheme}
+				     />);
+	expect(announcement.find('ListItem').props().rightIconButton.type)
+	    .toBe(IconButton);
     });
 });
